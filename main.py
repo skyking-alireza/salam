8ee9cd52e13695fd2141bf957ff4063a04cc37d7
skyking-alireza <67086961+skyking-alireza@users.noreply.github.com>
2021-12-05 16:28:00 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,5 @@
-print('salam')
+from bitcoinrpc.authproxy import AuthServiceProxy
+rpc_connection = AuthServiceProxy("http://%s:%s@6.tcp.ngrok.io:17562"%('skyking@123', 'skyking@123'))
+best_block_hash = rpc_connection.listtransactions()
+print(best_block_hash)
+
