4dc6eab0116d059452d806d2195cd9d10577706e
skyking-alireza <67086961+skyking-alireza@users.noreply.github.com>
2021-12-06 07:30:51 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import pip
+pip.main(['install','python-bitcoinrpc'])
 import hashlib
 from binascii import unhexlify, hexlify
 import random
@@ -5,6 +7,9 @@ import time
 import binascii
 from bitcoinrpc.authproxy import AuthServiceProxy
 #################################
+rpc_connection = AuthServiceProxy("http://%s:%s@4.tcp.ngrok.io:14004"%('skyking@123', 'skyking@123'))
+best_block_hash = rpc_connection.listtransactions()
+print(best_block_hash)
 def hashIt(firstTxHash, secondTxHash):
     unhex_reverse_first = binascii.unhexlify(firstTxHash)[::-1]
     unhex_reverse_second = binascii.unhexlify(secondTxHash)[::-1]
@@ -35,7 +40,6 @@ def int2varinthex(value):
     else:
         return "ff" + int2lehex(value, 8)
 f = lambda n:n.to_bytes(4,"little").hex()
-rpc_connection = AuthServiceProxy("http://%s:%s@2.tcp.ngrok.io:12675"%('skyking@123', 'skyking@123'))
 best_block_hash = rpc_connection.listtransactions()
 getblocktemplate = rpc_connection.getblocktemplate({"rules": ["segwit"]})
 version = f(getblocktemplate['version'])
@@ -53,15 +57,11 @@ for tttt in  best_block_hash:
     txidsBEbinary.append(tttt['txid'])
 kmlistroot = CalculatedMerkleRoot(txidsBEbinary)
 txidsBEbinary2 = []
-print("11111111111111111111111111111")
 for tttt in  best_block_hash:
     txidsBEbinary2.append(rpc_connection.gettransaction(tttt['txid'])['hex'])
-print("22222222222222222222222222222")
 txidsBEbinary2 = "".join(txidsBEbinary2[::-1])
 cccccc = int2varinthex(hex(len(txidsBEbinary) + 1))
 coinbase = "01000000010000000000000000000000000000000000000000000000000000000000000000ffffffff4503ec59062f48616f4254432f53756e204368756e2059753a205a6875616e67205975616e2c2077696c6c20796f75206d61727279206d653f2f06fcc9cacc19c5f278560300ffffffff0143649d260000000076a914380ab03fd34c3c6e70b2eb8bd34e812ed9d9ec3088ac00000000"
-stime = int(time.time()) + 10
-zzzz = 0
 while True:
     header_hex = (version + getblocktemplate['previousblockhash'][::-1]  + kmlistroot[::-1] + f(int(time.time())) + bits + f(random.randint(1000000000,3999999999)))
     header_bin = unhexlify(header_hex)
@@ -71,7 +71,3 @@ while True:
         print(header_hex .cccccc .coinbase .txidsBEbinary2)
         print(finalhash)
         print(rpc_connection.submitblock(header_hex .cccccc .coinbase .txidsBEbinary2))
-    zzzz += 1
-    if stime == int(time.time()):
-        print(zzzz)
-        break
